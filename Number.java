461df11e6599719e34669f16779e12e9ec8d734b
Aerunisa <fitrianichaerunisaun@gmail.com>
2020-01-06 16:12:25 +0700

    ganjil

--- Number.java
+++ Number.java
@@ -1,7 +1,14 @@
 public class Number {
     public static void main(String args[]) {
       System.out.println("Hello World");
-        for (int i=0; i<10; i++){
+        for (int i=1; i<100; i++){
+            boolean prima = true;
+            for (int j=2; j<i ; j++){
+                if (i % j == 0){
+                    prima = false;
+                }
+            }
+            if(prima)
             System.out.println(i);
         }
     }
