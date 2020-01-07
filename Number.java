dee0296ecd322051094bb605cf83da7e5f3ed8b0
Aerunisa <fitrianichaerunisaun@gmail.com>
2020-01-07 15:26:57 +0700

    last

--- Number.java
+++ Number.java
@@ -1,10 +1,6 @@
 public class Number {
     public static void main(String args[]) {
-<<<<<<< HEAD
       System.out.println("Hello Aeru These are prime number from 1 up to 100");
-=======
-      System.out.println("Hello Aeru Yhese are Prime Number from 1 up to 100");
->>>>>>> 9e1706530b38abaee63d972975eb299ddbae1ffa
         for (int i=1; i<100; i++){
             boolean prima = true;
             for (int j=2; j<i ; j++){
