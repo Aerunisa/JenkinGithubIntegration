9e1706530b38abaee63d972975eb299ddbae1ffa
Aerunisa <50533456+Aerunisa@users.noreply.github.com>
2020-01-07 14:41:11 +0700

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 public class Number {
     public static void main(String args[]) {
-      System.out.println("Hello World");
+      System.out.println("Hello Aeru Yhese are Prime Number from 1 up to 100");
         for (int i=1; i<100; i++){
             boolean prima = true;
             for (int j=2; j<i ; j++){
